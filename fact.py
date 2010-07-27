92971678ce3b26ef10b7678d650164458e819128
Emily Stumme <emstumme@gmail.com>
2010-07-27 10:30:10 -0700

    fixed infinite loop

--- fact.py
+++ fact.py
@@ -1,7 +1,15 @@
 # factorial function
 #
 #return the factorial of n. 
-# fact(m)=> n! => n* (n-1) + (n-2) *...* 1 
+# fact(m)=> n! => n* (n-1) + (n-2) *...* 1
+# fact(n) = n n * fact(n-1)
+# fact(5) = 5 * fact(4) 
 
 def fact(n):
-    return(n) 
+    if n == 0:
+        return 1
+    return n * fact(n- 1)
+
+def factone(n):
+    return 1 
+        
