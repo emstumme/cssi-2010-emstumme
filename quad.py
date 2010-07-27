24afbe949e22070ba7040e3da8a8f54b43ec7f1d
Emily Stumme <emstumme@gmail.com>
2010-07-26 17:47:56 -0700

    added working quadratic formula

--- quad.py
+++ quad.py
@@ -5,7 +5,9 @@
 # return the two roots of the equation as a tuple
 #
 # Example: qyad(1,3,-4) => (1,-4)
-
+import math 
 def quad(a,b,c):
     return (1, -4) # dummy implementation
 
+def quad(1,3,-4):
+    return (-b +math.sqrt(b**2 - 4*a*c)/2*a),(-b-math.sqrt(b**2-4*a*c)/2*a)
