611c83716ea3ae2b3f7883718d42963739b7a30b
Emily Stumme <emstumme@gmail.com>
2010-07-26 17:30:36 -0700

    added comment and dummy implementation

--- quad.py
+++ quad.py
@@ -1,1 +1,11 @@
 # quadratic equation solver
+#
+# given three constants a, b, and c
+# for the quadratic equation a x^2 +b x + c
+# return the two roots of the equation as a tuple
+#
+# Example: qyad(1,3,-4) => (1,-4)
+
+def quad(a,b,c):
+    return (1, -4) # dummy implementation
+
