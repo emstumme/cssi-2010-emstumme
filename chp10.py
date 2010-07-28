ad3bb479bd868046634f219aa2e4acd992df9684
Emily Stumme <emstumme@gmail.com>
2010-07-28 16:21:52 -0700

    chapter 10 exercises

--- chp10.py
+++ chp10.py
@@ -1,1 +1,13 @@
 # lists Chapter 10 
+
+def add_all(t):
+    """Add all the numbers in t and return the sum"""
+    total = 0
+    for x in t:
+        total +=x
+    return total
+
+# exercise 4
+
+def is_anagram(s, t):
+    """return True if s and t are anagrams"""
