49bcb8b619b5f625bd4c8319023d6b7374fbac7a
Emily Stumme <emstumme@gmail.com>
2010-07-29 10:35:34 -0700

    added changes

--- guess.py
+++ guess.py
@@ -1,3 +1,73 @@
 # guessing game
+#
+# >>>> guessee(100)
+# I have a number betwenn 1 and 100.
+# your guesas? 23
+# higher
+# your guess? 34
+# correct
+# >>>>
 
-def guessee():
+import random
+
+def guessee(maxvalue):
+    """ Thinks of a number between 1 and maxvalue, inclusive,
+and responds to guesses about what the number is"""
+    answer = random.randint(1, maxvalue)     
+
+    guess == raw_input("Please enter your guess")
+    int()
+    n == answer
+    g == guess
+    
+    if n > g:
+            print ("Higher")
+    elif g > n:
+            print ("Lower")        
+    else:
+        print ("You win" )
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
