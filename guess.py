5615070add9183159f9681640550ea10c3ac4af1
Emily Stumme <emstumme@gmail.com>
2010-07-30 09:58:21 -0700

    changed program, updated state

--- guess.py
+++ guess.py
@@ -26,6 +26,28 @@ and responds to guesses about what the number is"""
             guess = input("Guess Higher")
         elif g > n:
             print ("Lower")
-            guess = input("guess Higher")
+            guess = input("Guess Lower")
     else: 
             print ("You win" )
+
+
+
+def guesser(maxvalue)
+    """ Guesses the user's number between 1 and max value"""
+    low = 1
+    high = maxva;ue
+    while True:
+    guess = random.randint(low,maxvalue)
+    print "I guess " + str(guess)
+    response = raw_input("Did i get it?")
+    if response == "yes":
+        print "woo hoo"
+    elif response == "higher"
+        low = guess + 1
+    elif response == "lower":
+        high = guess - 1
+    else:
+        print "hmmm, I don't understand '" + response + "'"
+        
+
+    
