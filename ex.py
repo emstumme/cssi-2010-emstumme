7b9926ded2a8855dfd6c28a6513f294191d13ee2
Emily Stumme <emstumme@gmail.com>
2010-08-01 13:48:34 -0700

    working versions

--- ex.py
+++ ex.py
@@ -1,13 +1,10 @@
 
-# defines hangman, secret word, and amount of turns
-
 def hangman():
   print 'time to play hangman'
   secret_word = 'hello'
   guesses = ' '
   turns = 5
 
-#shows spaces for word
   while turns > 0:
     missed = 0
     for letter in secret_word:
@@ -18,27 +15,18 @@ def hangman():
         missed += 1
 
     print
-    
-#defines when you win
+
     if missed == 0:
       print 'You win!'
       break
-    
-#ask for letters 
+
     guess = raw_input('Please guess a letter: ')
     guesses += guess
-    
-# defines what happens when guess is wronf
+
     if guess not in secret_word:
       turns -= 1
       print 'Letter not in Word.'
       print turns, 'more turns'
       if turns == 0:
         print 'The answer is', secret_word
-
-         
-
-
-
-             
-
+      
